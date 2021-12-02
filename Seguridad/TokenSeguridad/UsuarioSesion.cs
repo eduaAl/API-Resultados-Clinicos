3c6c45837e1c339ab49210705c5123c3f78dadb6
Eduardo <espinozaparralese.eaep@gmail.com>
2021-12-01 21:54:48 -0600

    avances

---
+++ Seguridad/TokenSeguridad/UsuarioSesion.cs
@@ -0,0 +1,17 @@
+﻿using Aplicacion.Contratos;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Seguridad.TokenSeguridad
+{
+    class UsuarioSesion
+    {
+    }
+}
